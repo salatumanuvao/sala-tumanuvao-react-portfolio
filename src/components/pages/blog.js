512a3799395282b4e405b8e2ad776f94499de80f
Sala Tumanuvao <salatumanuvao1323@gmail.com>
2021-09-03 08:32:48 -0600

    Started adding infinite scroll feature

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
-export default function() {
+class Blog extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      blogItems: []
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.activateInfiniteScroll();
+  }
+
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        console.log("get more posts");
+      }
+    };
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://salatumanuvao.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          blogItems: response.data.portfolio_blogs
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
+
     return (
-        <div>
-            <h2>Blog</h2>
-
-            <div>
-                <Link to="/about-me">Read more about me</Link>
-            </div>
-        </div>
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
+      </div>
     );
+  }
 }
+
+export default Blog;
