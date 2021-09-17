2c7f43923f01e6307531b9e688652529458c263f
Sala Tumanuvao <salatumanuvao1323@gmail.com>
2021-09-16 18:34:53 -0600

    Finished first React app

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -6,12 +6,13 @@ import {
     faPlusCircle,
     faPhone,
     faEnvelope,
-    faMapMarkerAlt
+    faMapMarkerAlt,
+    faLock
   } from "@fortawesome/free-solid-svg-icons";
   import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, faEnvelope, faMapMarkerAlt);
+    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, faEnvelope, faMapMarkerAlt, faLock);
 };
 
 export default Icons;
